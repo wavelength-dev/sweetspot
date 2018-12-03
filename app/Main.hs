15a59bc8518d7d2155ae0f80dce2e700df35774d
Niclas Blomberg <niclas.blomberg@reaktor.com>
2018-12-03 20:21:34 +0100

    move code into Lib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,6 @@
-{-# LANGUAGE DataKinds #-}
-
 module Main where
 
-import           GHC.Generics
-import           LoadEnv
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           Servant
-import           System.Environment       (lookupEnv)
-import           System.IO
-
-type RootAPI = Get '[ PlainText] String
-
-helloWorldMessage :: String
-helloWorldMessage = "Hello World!"
-
-server1 :: Server RootAPI
-server1 = return helloWorldMessage
-
-rootAPI :: Proxy RootAPI
-rootAPI = Proxy
-
--- 'serve' comes from servant and hands you a WAI Application,
--- which you can think of as an "abstract" web application,
--- not yet a webserver.
-app1 :: Application
-app1 = serve rootAPI server1
+import Lib (runApp)
 
 main :: IO ()
-main = do
-  loadEnv
-  apiKey <- lookupEnv "SHOPIFY_API_KEY"
-  putStrLn $ show apiKey
-  run 8082 app1
+main = runApp
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,37 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DataKinds #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( runApp
+  ) where
+
+import GHC.Generics
+import LoadEnv
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import System.Environment (lookupEnv)
+import System.IO
+
+type RootAPI = Get '[ PlainText] String
+
+helloWorldMessage :: String
+helloWorldMessage = "Hello World!"
+
+server1 :: Server RootAPI
+server1 = return helloWorldMessage
+
+rootAPI :: Proxy RootAPI
+rootAPI = Proxy
+
+-- 'serve' comes from servant and hands you a WAI Application,
+-- which you can think of as an "abstract" web application,
+-- not yet a webserver.
+app :: Application
+app = serve rootAPI server1
+
+runApp :: IO ()
+runApp = do
+  loadEnv
+  apiKey <- lookupEnv "SHOPIFY_API_KEY"
+  putStrLn $ show apiKey
+  run 8082 app

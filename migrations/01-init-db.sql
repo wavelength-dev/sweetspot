be22656cb8300fa08202ab7d7ab2688b8108dc4f
Niclas Blomberg <niclas.blomberg@reaktor.com>
2018-12-04 17:01:18 +0100

    add postgres

---
+++ migrations/01-init-db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE experiments (product_id integer not null, price integer not null);
+
+INSERT INTO experiments (product_id, price) VALUES (123, 199);
+INSERT INTO experiments (product_id, price) VALUES (124, 99);
+INSERT INTO experiments (product_id, price) VALUES (125, 29);
+INSERT INTO experiments (product_id, price) VALUES (126, 599);
+INSERT INTO experiments (product_id, price) VALUES (127, 100);
+INSERT INTO experiments (product_id, price) VALUES (128, 129);
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Lib
   ( runApp
   ) where
 
+import Control.Monad
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson (ToJSON)
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow (field, fromRow)
 import GHC.Generics
 import LoadEnv
 import Network.Wai
@@ -14,26 +21,50 @@ import Servant.Server.StaticFiles
 import System.Environment (lookupEnv)
 import System.IO
 
-type RootAPI = "hello" :> Get '[ PlainText] String :<|> "static" :> Raw
+type RootAPI
+   = "hello" :> Get '[ PlainText] String :<|> "static" :> Raw :<|> "experiments" :> Get '[ JSON] [Experiment]
+
+data Experiment = Experiment
+  { id :: Int
+  , price :: Int
+  } deriving (Generic, Show)
+
+instance ToJSON Experiment
+
+instance FromRow Experiment where
+  fromRow = Experiment <$> field <*> field
 
 helloWorldMessage :: String
 helloWorldMessage = "Hello World!"
 
-server :: Server RootAPI
-server = return helloWorldMessage :<|> serveDirectoryWebApp "./static"
+experimentsHandler :: Connection -> Handler [Experiment]
+experimentsHandler dbconn =
+  liftIO (query_ dbconn experimentsQuery :: IO [Experiment])
+  where
+    experimentsQuery = "select product_id, price from experiments;" :: Query
+
+server :: Connection -> Server RootAPI
+server dbconn =
+  return helloWorldMessage :<|> serveDirectoryWebApp "./static" :<|>
+  experimentsHandler dbconn
 
 rootAPI :: Proxy RootAPI
 rootAPI = Proxy
 
--- 'serve' comes from servant and hands you a WAI Application,
--- which you can think of as an "abstract" web application,
--- not yet a webserver.
-app :: Application
-app = serve rootAPI server
+createApp :: Connection -> Application
+createApp dbconn = serve rootAPI (server dbconn)
 
 runApp :: IO ()
 runApp = do
   loadEnv
   apiKey <- lookupEnv "SHOPIFY_API_KEY"
+  dbconn <-
+    connect
+      defaultConnectInfo
+        { connectHost = "localhost"
+        , connectDatabase = "supple"
+        , connectPort = 5432
+        , connectUser = "postgres"
+        }
   putStrLn $ show apiKey
-  run 8082 app
+  run 8082 (createApp dbconn)

ed3296019b3aa0188969237a15fbe3185ee9611c
Niclas Blomberg <niclas.blomberg@reaktor.com>
2018-12-09 16:30:30 +0100

    add test data to migration

--- migrations/01-init-db.sql
+++ migrations/01-init-db.sql
@@ -1,5 +1,10 @@
 CREATE TABLE users (user_id integer primary key);
-CREATE TABLE buckets (variant_id integer primary key, sku text, price integer);
-CREATE TABLE user_buckets (user_id integer, variant_id integer,
+CREATE TABLE buckets (variant_id bigint primary key, sku text, price integer);
+CREATE TABLE user_buckets (user_id integer, variant_id bigint,
   foreign key (user_id) references users(user_id),
   foreign key (variant_id) references buckets(variant_id));
+
+
+INSERT INTO users (user_id) VALUES (123);
+INSERT INTO buckets (variant_id, sku, price) VALUES (17043734724672, 'MEH5680S', 24);
+INSERT INTO user_buckets (user_id, variant_id) VALUES (123, 17043734724672);

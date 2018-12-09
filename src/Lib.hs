469ef18f7ce9250292f802281b2a482109e41861
Niclas Blomberg <niclas.blomberg@reaktor.com>
2018-12-09 16:09:23 +0100

    add `get user bucket` route

--- src/Lib.hs
+++ src/Lib.hs
@@ -43,17 +43,34 @@ data BucketRes = BucketRes
   { message :: Text
   } deriving (Generic, Show)
 
+data UserBucketReq = UserBucketReq
+  { user_id :: Int
+  , product_sku :: Text
+  } deriving (Generic, Show)
+
+data UserBucketRes = UserBucketRes
+  { bucket_price :: Int
+  , bucket_variant :: Int
+  } deriving (Generic, Show)
+
 instance ToJSON BucketReq
 
 instance FromJSON BucketReq
 
 instance ToJSON BucketRes
 
+instance FromJSON UserBucketReq
+
+instance ToJSON UserBucketRes
+
 instance FromRow BucketReq where
   fromRow = BucketReq <$> field <*> field <*> field
 
+instance FromRow UserBucketRes where
+  fromRow = UserBucketRes <$> field <*> field
+
 type RootAPI
-   = "static" :> Raw :<|> "bucket" :> ReqBody '[ JSON] BucketReq :> Post '[ JSON] BucketRes
+   = "static" :> Raw :<|> "bucket" :> ReqBody '[ JSON] BucketReq :> Post '[ JSON] BucketRes :<|> "bucket" :> QueryParam "uid" Int :> QueryParam "sku" Text :> Get '[JSON] UserBucketRes
 
 createBucketHandler :: Connection -> BucketReq -> Handler BucketRes
 createBucketHandler dbconn req = do
@@ -66,8 +83,16 @@ createBucketHandler dbconn req = do
     q =
       "insert into buckets (variant_id, sku, price) values (?, ?, ?);" :: Query
 
+getUserBucketHandler :: Connection -> Maybe Int -> Maybe Text -> Handler UserBucketRes
+getUserBucketHandler dbconn (Just uid) (Just sku) = do
+  res <- liftIO $ (query dbconn q (uid, sku) :: IO [UserBucketRes])
+  return $ res !! 0
+  where
+    q = "select buckets.price, buckets.variant_id from user_buckets inner join users on user_buckets.user_id = users.user_id inner join buckets on user_buckets.variant_id = buckets.variant_id where users.user_id = ? and buckets.sku = ?;" :: Query
+getUserBucketHandler _ _ _ = undefined
+
 server :: Connection -> Server RootAPI
-server dbconn = serveDirectoryWebApp "./static" :<|> createBucketHandler dbconn
+server dbconn = serveDirectoryWebApp "./static" :<|> createBucketHandler dbconn :<|> getUserBucketHandler dbconn
 
 rootAPI :: Proxy RootAPI
 rootAPI = Proxy

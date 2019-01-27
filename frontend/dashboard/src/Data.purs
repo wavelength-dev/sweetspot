c13e9f9d75b605943ba42363dc5ee35953e4c7df
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-01-27 18:07:20 +0100

    add some shit to dashboard

---
+++ frontend/dashboard/src/Data.purs
@@ -0,0 +1,39 @@
+module Data where
+
+import Prelude
+
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Decode (decodeJson, getField)
+import Data.Either (Either)
+import Data.Traversable (sequence)
+
+type Bucket =
+  { bucket_id :: Int
+  , price :: Number}
+
+type Experiment =
+  { exp_id :: Int
+  , sku :: String
+  , buckets :: Array Bucket }
+
+decodeBucket :: Json -> Either String Bucket
+decodeBucket json = do
+  obj <- decodeJson json
+  bucket_id <- getField obj "bucket_id"
+  price <- getField obj "price"
+  pure { bucket_id, price }
+
+decodeExperiment :: Json -> Either String Experiment
+decodeExperiment json = do
+  obj <- decodeJson json
+  exp_id <- getField obj "exp_id"
+  sku <- getField obj "sku"
+  arr <- getField obj "buckets"
+  buckets <- sequence $ decodeBucket <$> arr
+  pure { exp_id, sku, buckets }
+
+
+decodeResponse :: Json -> Either String (Array Experiment)
+decodeResponse json = do
+  arr <- decodeJson json
+  sequence $ decodeExperiment <$> arr
--- frontend/dashboard/src/Main.purs
+++ frontend/dashboard/src/Main.purs
@@ -1,12 +1,34 @@
 module Main where
 
 import Prelude
+
+import Affjax as AX
+import Affjax.ResponseFormat as ResponseFormat
+import App as App
+import Data (Experiment, decodeResponse)
+import Data.Array as A
+import Data.Either (Either(..))
 import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Class.Console (log)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
-import Button as B
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI B.myButton unit body
+  r <- fetchExperiments
+  case r of
+    Left s -> log $ "Fail: " <> s
+    Right arr -> (log <<< show <<< A.length) arr
+  runUI App.component unit body
+
+
+fetchExperiments :: Aff (Either String (Array Experiment))
+fetchExperiments =  do
+  res <- AX.get ResponseFormat.json "/api/experiments"
+  let decoded = case res.body of
+        Left _ -> Left "Err"
+        Right json -> decodeResponse json
+
+  pure decoded

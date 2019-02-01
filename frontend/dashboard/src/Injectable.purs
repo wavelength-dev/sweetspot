6fc9fe9d037316aafe021689fda7e8586a024278
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-02-01 16:24:35 +0100

    quick poc for injectable aff handling

--- frontend/dashboard/src/Injectable.purs
+++ frontend/dashboard/src/Injectable.purs
@@ -3,16 +3,18 @@ module Injectable where
 import Prelude
 
 import Data.Array (uncons)
-import Data.Either (Either(..))
+import Data.Either (Either(..), note)
 import Data.HTTP.Method (Method(..)) as Method
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import Effect.Aff (Aff, makeAff)
+import Effect.Aff (Aff, Canceler(..), launchAff_, makeAff, runAff_)
+import Effect.Class (liftEffect)
 import Effect.Console (log)
+import Effect.Exception (Error, error)
 import Web.Event.EventTarget (addEventListener, eventListener)
 import Web.XHR.EventTypes (error, load) as EventType
 import Web.XHR.ResponseType (string) as ResponseType
-import Web.XHR.XMLHttpRequest (open, response, send, toEventTarget, xmlHttpRequest)
+import Web.XHR.XMLHttpRequest (open, response, send, toEventTarget, xmlHttpRequest, abort)
 
 type ApiExperiment
   = {bucket_price :: Number, bucket_sku :: String, bucket_svid :: Number, user_id :: Number}
@@ -21,21 +23,24 @@ type Experiment
   = {price :: Number, sku :: String, svid :: Number}
 
 apiURL :: String
-apiURL = "http://71a81ddd.ngrok.io/api/bucket"
+apiURL = "http://localhost/api/bucket?uid=1"
 
-getExperiments :: Aff (Maybe String)
-getExperiments = do
-  xhr <- xmlHttpRequest ResponseType.string
-  successListener <- eventListener ?success
-  failureListener <- eventListener ?failure
-  addEventListener EventType.load successListener false (toEventTarget xhr)
-  addEventListener EventType.error failureListener false (toEventTarget xhr)
-  open (Left Method.GET) apiURL xhr
-  send xhr
+getExperiments :: Aff String
+getExperiments =
+  makeAff \cb ->
+    liftEffect $ do
+      xhr <- xmlHttpRequest ResponseType.string
+      successListener <- eventListener
+                         \_ -> do
+                            res <- response xhr
+                            cb $ note (error "Fail") res
+
+      addEventListener EventType.load successListener false (toEventTarget xhr)
+      open (Left Method.GET) apiURL xhr
+      send xhr
+      pure $ Canceler \_ -> liftEffect $ abort xhr
 
 main :: Effect Unit
-main = do
+main = launchAff_ $ do
   expsRes <- getExperiments
-  case expsRes of
-    (Just expsJSON) -> log expsJSON
-    Nothing -> log "No response!"
+  liftEffect $ log expsRes

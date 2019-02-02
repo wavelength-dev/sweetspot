1ff517b99c6d36897e72c6c5cd3abda6b5d97c5a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-02-02 15:29:29 +0100

    Add DOM ready Aff
    Waits for both the dom ready and getExperiments Aff before continuing.

--- frontend/dashboard/src/Injectable.purs
+++ frontend/dashboard/src/Injectable.purs
@@ -2,19 +2,26 @@ module Injectable where
 
 import Prelude
 
-import Data.Array (uncons)
 import Data.Either (Either(..), note)
 import Data.HTTP.Method (Method(..)) as Method
-import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Aff (Aff, Canceler(..), launchAff_, makeAff, runAff_)
+import Effect.Aff (Aff, Canceler(..), launchAff_, makeAff, nonCanceler, parallel, sequential)
+import Effect.Aff.Compat (EffectFnAff)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Effect.Exception (Error, error)
+import Effect.Exception (error)
 import Web.Event.EventTarget (addEventListener, eventListener)
-import Web.XHR.EventTypes (error, load) as EventType
+import Web.HTML (window)
+import Web.HTML.Event.EventTypes (domcontentloaded)
+import Web.HTML.HTMLDocument (toEventTarget) as HDoc
+import Web.HTML.Window (document)
+import Web.XHR.EventTypes (load) as EventType
 import Web.XHR.ResponseType (string) as ResponseType
-import Web.XHR.XMLHttpRequest (open, response, send, toEventTarget, xmlHttpRequest, abort)
+import Web.XHR.XMLHttpRequest (open, response, send, xmlHttpRequest, abort)
+import Web.XHR.XMLHttpRequest (toEventTarget) as XHR
+
+type URL = String
 
 type ApiExperiment
   = {bucket_price :: Number, bucket_sku :: String, bucket_svid :: Number, user_id :: Number}
@@ -35,12 +42,27 @@ getExperiments =
                             res <- response xhr
                             cb $ note (error "Fail") res
 
-      addEventListener EventType.load successListener false (toEventTarget xhr)
+      addEventListener EventType.load successListener false (XHR.toEventTarget xhr)
       open (Left Method.GET) apiURL xhr
       send xhr
       pure $ Canceler \_ -> liftEffect $ abort xhr
 
+getDOMReady :: Aff Unit
+getDOMReady = 
+  makeAff \cb ->
+    liftEffect $ do
+      listener <- eventListener (\_ -> cb (Right unit))
+      doc <- (window >>= document)
+      addEventListener domcontentloaded listener false (HDoc.toEventTarget doc)
+      -- pure $ Canceler \_ -> liftEffect $ removeEventListener domcontentloaded listener false (HDoc.toEventTarget doc)
+      pure nonCanceler
+
+whenReady :: Aff (Tuple Unit String)
+whenReady = sequential $
+  Tuple <$> parallel (getDOMReady)
+        <*> parallel (getExperiments)
+
 main :: Effect Unit
 main = launchAff_ $ do
-  expsRes <- getExperiments
-  liftEffect $ log expsRes
+  Tuple a b <- whenReady
+  liftEffect $ log b

aac949c0c8272e445857e35d2fae3e90b1846726
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-02-09 18:37:17 +0100

    formatting

--- frontend/injectable/logging.ts
+++ frontend/injectable/logging.ts
@@ -5,22 +5,20 @@ const DEBUG = true
 const noop = () => undefined
 
 const consoleLog = (msg: string, severity = "info") => {
-      switch (severity) {
-        case "error": {
-          console.error(msg)
-          break
-        }
-        case "info": {
-          console.log(msg)
-          break
-        }
-        case "warn": {
-          console.warn(msg)
-          break
-        }
-      }
+  switch (severity) {
+    case "error": {
+      console.error(msg)
+      break
     }
+    case "info": {
+      console.log(msg)
+      break
+    }
+    case "warn": {
+      console.warn(msg)
+      break
+    }
+  }
+}
 
-export const log: LogFn = DEBUG
-  ? consoleLog
-  : noop
+export const log: LogFn = DEBUG ? consoleLog : noop

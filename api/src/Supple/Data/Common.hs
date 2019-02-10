2e6a4b0bc08326a836333bbd065aea4b95989bf7
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-02-10 15:57:26 +0100

    wrap sku and svid in newtypes

--- api/src/Supple/Data/Common.hs
+++ api/src/Supple/Data/Common.hs
@@ -3,12 +3,17 @@
 module Supple.Data.Common where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
 import Data.Scientific (Scientific)
 import GHC.Generics (Generic)
 
 newtype Price =
   Price Scientific
   deriving (Show, Generic)
+
+newtype Svid = Svid Int
+
+newtype Sku = Sku Text
 
 instance ToJSON Price
 

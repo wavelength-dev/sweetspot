6be9839873b02635c959eeb7d58c188941b238f4
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-02-23 13:50:38 +0100

    Add checkout injectable

---
+++ frontend/injectable/checkout.ts
@@ -0,0 +1,44 @@
+import { eventsUrl } from "./constants"
+import { detectPage } from "./events"
+import { log } from "./logging"
+
+const trackCheckout = (): void => {
+  log("Tracking checkout")
+  const page = detectPage()
+  if (page !== "checkout") {
+    log("Not a checkout page, skipping tracking")
+    return
+  }
+
+  const { step, token } =
+    typeof window.Shopify === "object" &&
+    typeof window.Shopify.Checkout === "object"
+      ? window.Shopify.Checkout
+      : { step: null, token: null }
+
+  const { line_items: lineItems } =
+    typeof window.Shopify === "object" &&
+    typeof window.Shopify.checkout === "object"
+      ? window.Shopify.checkout
+      : { line_items: null }
+
+  const pageUrl = window.location.href
+
+  fetch(eventsUrl, {
+    body: JSON.stringify({
+      page,
+      pageUrl,
+      step: step || null,
+      token: token || null,
+      lineItems: lineItems || null,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  }).catch(err => {
+    log(err, "error")
+  })
+}
+
+trackCheckout()

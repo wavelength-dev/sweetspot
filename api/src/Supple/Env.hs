c9c383324fdfaa2e89bf7c39105c60f48fc24315
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-02-24 11:19:17 +0100

    add env module

---
+++ api/src/Supple/Env.hs
@@ -0,0 +1,25 @@
+module Supple.Env where
+
+import LoadEnv (loadEnv)
+import System.Envy (FromEnv, (.!=), decodeEnv, envMaybe, fromEnv)
+
+data EnvConfig = EnvConfig
+  { dbHost :: String
+  , dbName :: String
+  , dbPassword :: String
+  , dbPort :: Int
+  , dbUser :: String
+  } deriving (Show)
+
+instance FromEnv EnvConfig where
+  fromEnv =
+    EnvConfig <$> envMaybe "DB_HOST" .!= "localhost" <*>
+    envMaybe "DB_NAME" .!= "supple" <*>
+    envMaybe "DB_PASSWORD" .!= "" <*>
+    envMaybe "DB_PORT" .!= 5432 <*>
+    envMaybe "DB_USER" .!= "supple"
+
+getEnvConfig :: IO (Either String EnvConfig)
+getEnvConfig = do
+  loadEnv
+  decodeEnv :: IO (Either String EnvConfig)

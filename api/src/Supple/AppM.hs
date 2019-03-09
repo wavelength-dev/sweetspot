608fb06a1fdbc0a8d0d43de0b098bab8bc25cb66
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-03-09 16:13:54 +0100

    add log level

--- api/src/Supple/AppM.hs
+++ api/src/Supple/AppM.hs
@@ -3,21 +3,11 @@
 module Supple.AppM where
 
 import Control.Monad.Reader (ReaderT)
-import Data.Aeson
-  ( FromJSON
-  , ToJSON
-  , defaultOptions
-  , encode
-  , genericToEncoding
-  , toEncoding
-  )
 import Data.Text (Text)
-import Data.Time.Clock (UTCTime)
 import GHC.Generics (Generic)
 import Servant (Handler)
 import Supple.Database (Connection)
 import System.Log.FastLogger (LoggerSet)
-import System.Log.FastLogger (ToLogStr(..))
 
 data AppConfig = AppConfig
   { environment :: !Text
@@ -29,18 +19,5 @@ data AppCtx = AppCtx
   , _getLogger :: !LoggerSet
   , _getDbConn :: !Connection
   }
-
-data LogMessage = LogMessage
-  { message :: !Text
-  , timestamp :: !UTCTime
-  } deriving (Eq, Show, Generic)
-
-instance FromJSON LogMessage
-
-instance ToJSON LogMessage where
-  toEncoding = genericToEncoding defaultOptions
-
-instance ToLogStr LogMessage where
-  toLogStr = toLogStr . encode
 
 type AppM = ReaderT AppCtx Handler
--- api/src/Supple/Logger.hs
+++ api/src/Supple/Logger.hs
@@ -1,16 +1,65 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-module Supple.Logger where
+module Supple.Logger
+  ( info
+  , warn
+  , error
+  ) where
 
-import Data.Text (Text)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (asks)
-import Data.Time.Clock (getCurrentTime)
-import Supple.AppM (AppM, LogMessage(..), AppCtx(..))
+import Data.Aeson
+  ( ToJSON(..)
+  , defaultOptions
+  , encode
+  , genericToEncoding
+  , toEncoding
+  )
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime, getCurrentTime)
+import GHC.Generics (Generic)
+import Prelude hiding (log, error)
+import Supple.AppM (AppCtx(..), AppM)
 import System.Log.FastLogger (ToLogStr(..), pushLogStrLn)
 
-log :: Text -> AppM ()
-log msg = do
+data LogLevel
+  = Info
+  | Warn
+  | Error
+  deriving (Eq, Show)
+
+instance ToJSON LogLevel where
+  toJSON lvl = case lvl of
+    Info -> "info"
+    Warn -> "warn"
+    Error -> "error"
+
+data LogMessage = LogMessage
+  { level :: !LogLevel
+  , message :: !Text
+  , timestamp :: !UTCTime
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON LogMessage where
+  toEncoding = genericToEncoding defaultOptions
+
+instance ToLogStr LogMessage where
+  toLogStr = toLogStr . encode
+
+log :: LogLevel -> Text -> AppM ()
+log lvl msg = do
   logset <- asks _getLogger
   ts <- liftIO getCurrentTime
-  liftIO $ pushLogStrLn logset $ toLogStr LogMessage {message = msg, timestamp = ts}
+  liftIO $
+    pushLogStrLn logset $
+    toLogStr LogMessage {level = lvl, message = msg, timestamp = ts}
+
+info :: Text -> AppM ()
+info msg = log Info msg
+
+warn :: Text -> AppM ()
+warn msg = log Warn msg
+
+error :: Text -> AppM ()
+error msg = log Error msg

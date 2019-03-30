8a72900122659c8cccb3ca32a7daddfd69443db9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-03-30 11:05:11 +0100

    get rid of old TS injectable

--- injectable/constants.ts
+++
@@ -1,4 +0,0 @@
-const apiURL = "https://app-staging.getsweetspot.com/api/"
-export const experimentsURL = `${apiURL}bucket`
-export const eventsURL = `${apiURL}event`
-export const logURL = `${apiURL}log`

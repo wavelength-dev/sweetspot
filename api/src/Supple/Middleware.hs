164acf76eac1eda5b6725ac4fb89ebe56b968167
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-03-30 16:25:48 +0100

    add basic auth to dashboard routes

---
+++ api/src/Supple/Middleware.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Supple.Middleware
+  ( appMiddleware
+  ) where
+
+import Network.Wai (Middleware)
+import Network.Wai.Middleware.Cors
+  ( cors
+  , corsExposedHeaders
+  , corsMethods
+  , corsOrigins
+  , corsRequestHeaders
+  , simpleCorsResourcePolicy
+  , simpleHeaders
+  , simpleMethods
+  )
+import Network.Wai.Middleware.Gzip
+  ( GzipFiles(GzipCacheFolder)
+  , def
+  , gzip
+  , gzipFiles
+  )
+import Network.Wai.Middleware.HttpAuth (basicAuth)
+import Network.Wai.Middleware.Routed (routedMiddleware)
+
+-- WAI doesn't seem to want to know about routing.
+-- This should probably move into a Servant handler somehow.
+gzipStatic :: Middleware
+gzipStatic = routedMiddleware ("static" `elem`) (gzip settings)
+  where
+    settings = def {gzipFiles = GzipCacheFolder "../dist/"}
+
+auth :: Middleware
+auth = routedMiddleware ("dashboard" `elem`) mw
+  where
+    check = (\u p -> return $ u == "sweetspot" && p == "***REMOVED***")
+    mw = basicAuth check "Dashboard realm"
+
+corsMiddleware :: Middleware
+corsMiddleware =
+  cors $ \_ ->
+    Just $
+    simpleCorsResourcePolicy
+      { corsOrigins =
+          Just
+            ( [ "https://kamikoto.com"
+              , "https://libertyprice.myshopify.com"
+              , "http://localhost:8082"
+              ]
+            , True)
+      , corsRequestHeaders = "Content-Type" : simpleHeaders
+      , corsMethods = simpleMethods
+      , corsExposedHeaders =
+          Just ["Set-Cookie", "Access-Control-Allow-Origin", "Content-Type"]
+      }
+
+appMiddleware :: Middleware
+appMiddleware = corsMiddleware . gzipStatic . auth

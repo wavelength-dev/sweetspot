12d6835e8801b6a44960917aab9b619fab59a68f
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-05-30 11:40:10 +0200

    remove test data from init script

--- api/migrations/01-init-db.sql
+++ api/migrations/01-init-db.sql
@@ -39,11 +39,3 @@ CREATE TABLE events
  type TEXT,
  timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
  payload JSONB);
-
-INSERT INTO users (user_id) VALUES (DEFAULT);
-INSERT INTO experiment_groups (exp_group_id) VALUES (DEFAULT);
-INSERT INTO experiments (exp_id, sku, name, campaign_id, min_profit_increase) VALUES (DEFAULT, '714449933422', 'Kanpeki knife experiment', 'knp123', 10);
-INSERT INTO buckets (bucket_id, bucket_type, svid, sku, price) VALUES (DEFAULT, 'control', 12502066561095, '714449933422', 197.90);
-INSERT INTO buckets (bucket_id, bucket_type, svid, sku, price) VALUES (DEFAULT, 'test', 12502066561095, '714449933422', 249.90);
-INSERT INTO experiment_buckets (exp_id, bucket_id) VALUES (1, 1);
-INSERT INTO experiment_buckets (exp_id, bucket_id) VALUES (1, 2);

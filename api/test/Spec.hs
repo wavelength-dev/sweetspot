dc913d47ce8c47c27a0b54528e8adaa946ee7730
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-06-01 17:53:02 +0200

    Add the first set of api tests

--- api/test/Spec.hs
+++ api/test/Spec.hs
@@ -19,7 +19,7 @@ import Servant
 import Servant.Client
 import Servant.Server
 import Test.Hspec
-import Test.Hspec.Wai
+import Test.Hspec.Wai hiding (pending)
 import Test.Hspec.Wai.Matcher
 
 import Supple.Data.Api
@@ -34,11 +34,10 @@ magicWaitNumber = 3 * 1000 * 1000
 beforeSetup :: IO ()
 beforeSetup = runInThread >> C.threadDelay magicWaitNumber
   where
-    runInThread = liftIO $ C.forkIO $ runServer
+    runInThread = liftIO $ C.forkIO runServer
 
 businessLogicSpec :: Spec
 businessLogicSpec =
-  -- `around` will start our Server before the tests and turn it off after
   beforeAll_ beforeSetup . before_ reset $ do
     let getBucket :<|> postEvent :<|> postLog
           = client (Proxy :: Proxy InjectableAPI)
@@ -46,15 +45,44 @@ businessLogicSpec =
     manager <- runIO $ newManager defaultManagerSettings
     let clientEnv = mkClientEnv manager baseUrl
 
-    -- testing scenarios start here
     describe "GET /api/bucket" $ do
-      it "should get bucket for existing user" $ do
+      it "should get buckets for an existing user" $ do
         result <- runClientM (getBucket Nothing (Just 1000)) clientEnv
         case result of
-          Right bs -> bs ^?! ix 0 ^. ubUserId `shouldBe` (UserId 1000)
-          Left err -> do
-            print err
-            True `shouldBe` False
+          Left err -> error (show err)
+          Right bs -> bs ^?! ix 0 ^. ubUserId `shouldBe` UserId 1000
+
+      it "should create a new user when given a valid campaign id" $ do
+        result <- runClientM (getBucket (Just "longv123") Nothing) clientEnv
+        case result of
+          Left err -> error (show err)
+          Right _ -> return ()
+
+      it "should not return buckets for invalid campaign ids" $ do
+        result <- runClientM (getBucket (Just "unknown_campaign") Nothing) clientEnv
+        case result of
+          Left (FailureResponse res) -> responseStatusCode res `shouldBe` status400
+          Left err -> error (show err)
+          Right _ -> expectationFailure "expected request to fail"
+
+      it "should return buckets for invalid campaign ids for known users" $ do
+        result <- runClientM (getBucket (Just "unknown_campaign") (Just 1000)) clientEnv
+        case result of
+          Left err -> error (show err)
+          Right _ -> return ()
+
+      it "should not return buckets for unknown user ids" $ do
+        result <- runClientM (getBucket (Just "unknown_campaign") (Just 9001)) clientEnv
+        case result of
+          Left (FailureResponse res) -> responseStatusCode res `shouldBe` status404
+          Left err -> error (show err)
+          Right _ -> expectationFailure "expected request to fail"
+
+      it "should keep the same user id when given a valid campaignId and userId" $ do
+        result <- runClientM (getBucket (Just "longv123") (Just 1000)) clientEnv
+        case result of
+          Left err -> error (show err)
+          Right bs -> bs ^?! ix 0 ^. ubUserId `shouldBe` UserId 1000
 
 main :: IO ()
 main = hspec businessLogicSpec

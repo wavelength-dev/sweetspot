79923ea5803d309a38cc2f593f3fba6e68beb13e
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-06-23 11:48:24 +0200

    read all consts from env

--- injps/src/SweetSpot/Env.js
+++ injps/src/SweetSpot/Env.js
@@ -1,2 +1,6 @@
 "use strict";
 exports.apiURL = process.env.API_URL || "/apps/sweetspot/api"
+exports.hiddenPriceId = process.env.HIDDEN_PRICE_ID || "sweetspot__price--hidden"
+exports.productClass = process.env.PRODUCT_CLASS || "sweetspot__product"
+exports.uidStorageKey = process.env.UID_STORAGE_KEY || "sweetspot_uid_1554053013187"
+exports.idClassPattern = process.env.ID_CLASS_PATTERN || "sweetspot__price_id--"

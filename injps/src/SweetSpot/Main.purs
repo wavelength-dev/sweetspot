eead6091c5cc07858ac10f9d07a3230396b58a9a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-07-13 16:10:39 +0200

    Make price unhiding more robust

--- injps/src/SweetSpot/Main.purs
+++ injps/src/SweetSpot/Main.purs
@@ -1,19 +1,35 @@
 module SweetSpot.Main where
 
 import Prelude
-
+import Data.Array (catMaybes, length)
 import Data.Array.NonEmpty (head)
 import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
+import Data.Foldable (traverse_)
 import Effect (Effect)
-import Effect.Aff (forkAff, launchAff_)
+import Effect.Aff (Aff, launchAff_)
 import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
 import SweetSpot.AppM (AppM, ClientErr(..), runAppM)
-import SweetSpot.Capability (applyPriceVariations, attachPriceObserver, ensureCampaign, ensureDeps, getUserBuckets, getUserId, log, setUserId)
-import SweetSpot.DOM (getDOMReady, unhidePrice)
+import SweetSpot.Capability (applyPriceVariations, attachPriceObserver, ensureCampaign, ensureDeps, getUserBuckets, getUserId, setUserId)
+import SweetSpot.DOM (collectPriceEls, getDOMReady, removeClass)
+import SweetSpot.Data.Constant (hiddenPriceId)
 import SweetSpot.Event (trackView)
 import SweetSpot.Request (postLogPayload)
+import Web.HTML.HTMLElement (fromElement)
+
+unhidePrice :: Effect Unit
+unhidePrice = do
+  priceElements <- collectPriceEls
+  let
+    priceHTMLElements = catMaybes $ map fromElement priceElements
+  -- It's unlikely but possible not all collected Elements are HTMLElements
+  -- We should only add sweetspot ids to elements which are HTMLElements
+  -- In case we made a mistake, we log a warning and continue with those elements which are HTMLElements
+  _ <- if length priceElements /= length priceHTMLElements then
+    launchAff_ $ postLogPayload "WARN: some collected price elements are not HTMLElements"
+  else
+    pure unit
+  traverse_ (removeClass hiddenPriceId) priceHTMLElements
 
 app :: AppM Unit
 app = do
@@ -23,18 +39,16 @@ app = do
   campaignId <- ensureCampaign uid
   buckets <- getUserBuckets uid campaignId
   setUserId (head buckets)
-  mApplied <- applyPriceVariations buckets
-  case mApplied of
-    Nothing -> log "Unable to reveal prices for some collected price elements"
-    Just _ -> pure unit
+  applyPriceVariations buckets
   attachPriceObserver buckets
   trackView (head buckets)
 
-main :: Effect Unit
-main = launchAff_ $ do
-  result <- runAppM app
-  liftEffect $ case result of
+main :: Aff Unit
+main = do
+  result <- (runAppM app)
+  case result of
     Right _ -> pure unit
     Left (ClientErr { message }) -> do
-      _ <- unhidePrice
-      launchAff_ $ forkAff $ postLogPayload message
+      _ <- liftEffect $ unhidePrice
+      -- If posting this log message fails there is little more we can do to report it so we ignore the result.
+      liftEffect $ launchAff_ $ postLogPayload message

64a2a1ac1dd428c21d70fb84a7ec35e8c7968fc0
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-07-13 15:30:54 +0200

    Formatting

--- injps/src/SweetSpot/Data/Constant.purs
+++ injps/src/SweetSpot/Data/Constant.purs
@@ -3,29 +3,23 @@ module SweetSpot.Data.Constant where
 import Prelude ((<>))
 import SweetSpot.Env as Env
 
-hiddenPriceId ::
-  String
+hiddenPriceId :: String
 hiddenPriceId = Env.hiddenPriceId
 
-productClass ::
-  String
+productClass :: String
 productClass = Env.productClass
 
-uidStorageKey ::
-  String
+uidStorageKey :: String
 uidStorageKey = Env.uidStorageKey
 
 idClass :: String
 idClass = Env.idClass
 
-eventEndpoint ::
-  String
+eventEndpoint :: String
 eventEndpoint = Env.apiURL <> "/event"
 
-logEndpoint ::
-  String
+logEndpoint :: String
 logEndpoint = Env.apiURL <> "/log"
 
-experimentEndpoint ::
-  String
+experimentEndpoint :: String
 experimentEndpoint = Env.apiURL <> "/bucket"

8bef518e6a4277e2a5b31b1ac53ab119d19f58b8
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-08-11 17:18:29 +0200

    remove timestamp from local storage key

--- injps/src/SweetSpot/Env.js
+++ injps/src/SweetSpot/Env.js
@@ -2,5 +2,5 @@
 exports.apiURL = process.env.API_URL || "/apps/sweetspot/api"
 exports.hiddenPriceId = process.env.HIDDEN_PRICE_ID || "sweetspot__price--hidden"
 exports.productClass = process.env.PRODUCT_CLASS || "sweetspot__product"
-exports.uidStorageKey = process.env.UID_STORAGE_KEY || "sweetspot_uid_1554053013187"
+exports.uidStorageKey = process.env.UID_STORAGE_KEY || "sweetspot_uid"
 exports.idClass = process.env.ID_CLASS || "sweetspot__price_id--"

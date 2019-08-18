5b33063a6e21816859dcc9b8e26f878269a3bba4
Niclas Blomberg <niclas.blomberg@reaktor.com>
2019-08-18 13:23:41 +0200

    replace createExperiment

--- api/src/SweetSpot/Database/Queries/Dashboard.hs
+++ api/src/SweetSpot/Database/Queries/Dashboard.hs
@@ -7,12 +7,75 @@
 
 module SweetSpot.Database.Queries.Dashboard where
 
+import           Control.Lens
 import           Data.Text                      ( Text )
 import           Database.Beam
+import           Database.Beam.Backend.SQL.BeamExtensions
+                                               as BeamExt
 import           Database.Beam.Postgres
 
+import           SweetSpot.Data.Common
 import           SweetSpot.Database.Schema
 
+createExperiment
+        :: Connection
+        -> (Sku, Svid, Svid, Price, Price, CampaignId, Text)
+        -> IO ()
+createExperiment conn (Sku s, Svid ctrl, Svid test, Price ctrlP, Price testP, CampaignId c, name)
+        = do
+                [exp] <-
+                        runBeamPostgres conn
+                        $ BeamExt.runInsertReturningList
+                        $ insert (db ^. experiments)
+                        $ insertExpressions
+                                  [ Experiment { _expId          = default_
+                                               , _expSku         = val_ s
+                                               , _expProductName = val_ name
+                                               }
+                                  ]
+
+                runBeamPostgres conn
+                        $ runInsert
+                        $ insert (db ^. campaignExperiments)
+                        $ insertValues
+                                  [ CampaignExperiment
+                                            { _cmpForExp = CampaignKey c
+                                            , _expForCmp = toExpKey exp
+                                            }
+                                  ]
+                [cb, tb] <-
+                        runBeamPostgres conn
+                        $ BeamExt.runInsertReturningList
+                        $ insert (db ^. buckets)
+                        $ insertExpressions
+                                  [ Bucket { _bktId       = default_
+                                           , _bktType     = val_ "control"
+                                           , _bktCtrlSvid = val_ ctrl
+                                           , _bktTestSvid = val_ ctrl
+                                           , _bktPrice    = val_ ctrlP
+                                           }
+                                  , Bucket { _bktId       = default_
+                                           , _bktType     = val_ "test"
+                                           , _bktCtrlSvid = val_ ctrl
+                                           , _bktTestSvid = val_ test
+                                           , _bktPrice    = val_ testP
+                                           }
+                                  ]
+
+                runBeamPostgres conn
+                        $ runInsert
+                        $ insert (db ^. experimentBuckets)
+                        $ insertValues
+                                  [ ExperimentBucket { _expForBkt = toExpKey exp
+                                                     , _bktForExp = toBktKey cb
+                                                     }
+                                  , ExperimentBucket { _expForBkt = toExpKey exp
+                                                     , _bktForExp = toBktKey tb
+                                                     }
+                                  ]
+    where
+        toExpKey exp = exp ^. expId & ExperimentKey
+        toBktKey bkt = bkt ^. bktId & BucketKey
 
 getCampaign :: Connection -> Text -> IO [Campaign]
 getCampaign conn cmpId =

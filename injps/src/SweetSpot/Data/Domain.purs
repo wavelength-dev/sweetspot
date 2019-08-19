64681eac8c95e5f71d907bf2f3797d79d5b1e5a6
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-08-19 19:09:56 +0200

    add comments explaining domain types

--- injps/src/SweetSpot/Data/Domain.purs
+++ injps/src/SweetSpot/Data/Domain.purs
@@ -2,9 +2,15 @@ module SweetSpot.Data.Domain where
 
 import Data.Newtype (class Newtype)
 
+-- | Id which corresponds to a SweetSpot campaign. Campaigns are time bound events within which price tests take place.
 newtype CampaignId
   = CampaignId String
 
+-- | Svid's are id's used to identify variants.
+newtype Svid
+  = Svid String
+
+-- | UserIds are id's assigned to users within the SweetSpot system.
 newtype UserId
   = UserId String
 

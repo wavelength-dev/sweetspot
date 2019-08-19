fb043b191420b27f1ec99ce2020d3b13ce3cc46e
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-08-19 21:28:55 +0200

    make maximum fraction digits 0

--- injps/src/SweetSpot/Intl.purs
+++ injps/src/SweetSpot/Intl.purs
@@ -7,6 +7,6 @@ foreign import data NumberFormat :: Type
 foreign import _numberFormat :: forall a. String -> a -> Effect NumberFormat
 
 numberFormat :: Effect NumberFormat
-numberFormat = _numberFormat "en-US" { style: "currency", currency: "USD" }
+numberFormat = _numberFormat "en-US" { style: "currency", currency: "USD", maximumFractionDigits: 0 }
 
 foreign import formatNumber :: Number -> NumberFormat -> Effect String

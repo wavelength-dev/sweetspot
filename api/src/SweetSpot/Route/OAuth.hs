62cf80facc763bde4f90e6f5100cfda5439616d4
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-09-09 00:23:22 +0200

    add some explanatory comments to oauth handler

--- api/src/SweetSpot/Route/OAuth.hs
+++ api/src/SweetSpot/Route/OAuth.hs
@@ -16,12 +16,28 @@ import qualified SweetSpot.Logger              as L
 import           SweetSpot.Route.Util           ( internalServerErr )
 import           SweetSpot.ShopifyClient        ( exchangeAccessToken )
 
-type RedirectRoute
-  = "redirect" :> QueryParam "code" Text :> QueryParam "hmac" Text :> QueryParam "timestamp" Text :> QueryParam "state" Text :> QueryParam "shop" Text :> Get '[JSON] OkResponse
+-- See: https://help.shopify.com/en/api/getting-started/authentication/oauth
+-- The below handler handles Shopify redirecting the merchant to us, with an authorization_code is the URL. We use that authorization_code to then make a request to Shopify that gets us an access token for the given store.
+-- TODO: Make the nonce a randomly selected value checked against the nonce in the oauth callback
 
-type OAuthAPI = "oauth" :> RedirectRoute
+-- LibertyPrice
+-- scopes read_products,write_products,read_orders,read_analytics
+-- redirect_uri https://app-staging.getsweetspot.com/api/oauth/redirect
+-- nonce 1123
+--
+-- Longvadon
+-- scopes read_products,write_products,read_orders,read_analytics
+-- redirect_uri https://app.getsweetspot.com/api/oauth/redirect
+-- nonce 1123
 
--- | TODO: implement this properly
+type OAuthAPI = "oauth" :> "redirect"
+  :> QueryParam "code" Text
+  :> QueryParam "hmac" Text
+  :> QueryParam "timestamp" Text
+  :> QueryParam "state" Text
+  :> QueryParam "shop" Text
+  :> Get '[JSON] OkResponse
+
 redirectHandler
   :: Maybe Text
   -> Maybe Text
@@ -29,12 +45,11 @@ redirectHandler
   -> Maybe Text
   -> Maybe Text
   -> AppM OkResponse
-redirectHandler code hmac timestamp state shop = do
-  case code of
-    Just c -> do
-      permCode <- exchangeAccessToken c
-      L.info $ "Got code: " <> permCode
-      return $ OkResponse { message = "Authenticated app" }
-    Nothing -> throwError internalServerErr
+redirectHandler code hmac timestamp state shop = case code of
+  Just c -> do
+    permCode <- exchangeAccessToken c
+    L.info $ "Got code: " <> permCode
+    return $ OkResponse { message = "Authenticated app" }
+  Nothing -> throwError internalServerErr
 
 oauthHandler = redirectHandler

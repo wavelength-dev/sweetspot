ed10185dfaf10fef079b49ead69dab558fedb743
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-09-22 17:34:42 +0200

    avoid double quoting

--- injectable/src/Logging.purs
+++ injectable/src/Logging.purs
@@ -1,15 +1,18 @@
-module SweetSpot.Logging (class LogAction, log, LogLevel(..)) where
+module SweetSpot.Logging (class LogAction, log, buildLog, LogLevel(..)) where
 
 import Prelude
 
 import Data.Argonaut (class EncodeJson, Json, (:=), (~>))
-import Data.Argonaut (encodeJson, jsonEmptyObject) as Argonaut
+import Data.Argonaut (encodeJson, jsonEmptyObject, stringify) as Argonaut
 import Data.Either (Either(..))
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow) as Generic
 import Effect (Effect)
-import Effect.Aff (runAff_) as Aff
+import Effect.Aff (attempt, launchAff_) as Aff
+import Effect.Class (liftEffect)
 import Effect.Console (error) as Console
+import Effect.Exception (error)
+import Milkis (statusCode) as Milkis
 import SweetSpot.Api (sendLog) as Api
 
 data LogLevel
@@ -27,17 +30,25 @@ instance encodeJsonLogLevel :: EncodeJson LogLevel where
   encodeJson Warn = Argonaut.encodeJson "warn"
   encodeJson Error = Argonaut.encodeJson "error"
 
-class Monad m <= LogAction m where
-  log :: forall a. Show a => LogLevel -> a -> m Unit
+class
+  Monad m <= LogAction m where
+  log :: forall a. EncodeJson a => LogLevel -> a -> m Unit
 
 instance monadLogEffect :: LogAction Effect where
-  log level message = Aff.runAff_ onLogResult $ Api.sendLog (buildLog level message)
+  log level message = Aff.launchAff_ do
+      _response <- Aff.attempt $ Api.sendLog (buildLog level message)
+      liftEffect $ case _response of
+        Left logPostingErr -> Console.error $ format logPostingErr
+        Right response ->
+          if (Milkis.statusCode response) == 200 then
+            pure unit
+          else
+            Console.error $ format logFailedErr
     where
-    -- If we fail to communicate why we short-circuted to the server, we fallback to logging to console.
-    onLogResult result = case result of
-      Right _ -> pure unit
-      Left logPostingErr -> Console.error $ format logPostingErr
+    logFailedErr = error "Server responded with statusCode != 200 for log request"
 
+    -- If we fail to communicate why we short-circuted to the server, we fallback to logging to console.
+    format :: forall a. Show a => a -> String
     format logErr =
       "Failed to post log message, falling back to console.\n"
         <> "Logging error: "
@@ -45,11 +56,11 @@ instance monadLogEffect :: LogAction Effect where
         <> "App log, level: "
         <> show level
         <> ", message: "
-        <> show message
+        <> (message # Argonaut.encodeJson >>> Argonaut.stringify)
 
-buildLog :: forall a. Show a => LogLevel -> a -> Json
+buildLog :: forall a. EncodeJson a => LogLevel -> a -> Json
 buildLog level message =
-  "message" := show message
+  "message" := message
     ~> "level"
     := level
     ~> Argonaut.jsonEmptyObject

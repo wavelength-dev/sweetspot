f82c8096f092f1778de48c820668ca5489797b76
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-09-22 13:10:22 +0200

    add partly implemented log monad

--- injectable/src/Log.purs
+++ injectable/src/Log.purs
@@ -1,7 +1,8 @@
-module SweetSpot.Log (LogLevel(..)) where
+module SweetSpot.Log (class MonadLog, log, LogLevel(..)) where
 
 import Data.Argonaut (class EncodeJson)
 import Data.Argonaut (encodeJson) as Argonaut
+import Prelude (class Monad, class Show, Unit)
 
 data LogLevel = Info | Warn | Error
 
@@ -9,3 +10,6 @@ instance encodeJsonLogLevel :: EncodeJson LogLevel where
   encodeJson Info = Argonaut.encodeJson "info"
   encodeJson Warn = Argonaut.encodeJson "warn"
   encodeJson Error = Argonaut.encodeJson "error"
+
+class Monad m <= MonadLog m where
+  log :: forall a. Show a => a -> m Unit

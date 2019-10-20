682692c9fa44d425ef73031c62e8237a977dd7ee
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-10-20 17:56:27 +0200

    simplify RuntimeDependency tests

--- sprice/test/RuntimeDependency.purs
+++ sprice/test/RuntimeDependency.purs
@@ -2,13 +2,12 @@ module Test.RuntimeDependency where
 
 import Prelude
 
-import Control.Monad.Free (Free)
 import Effect.Class (liftEffect)
 import Sprice.RuntimeDependency (getIsRuntimeAdequate)
-import Test.Unit (TestF, test)
+import Test.Unit (TestSuite, test)
 import Test.Unit.Assert (assertFalse) as Assert
 
-tests :: Free TestF Unit
+tests :: TestSuite
 tests = test "finds runtime inadequate" do
   -- fetch will be missing on Node which is what the tests run on
   isRuntimeAdequate <- liftEffect getIsRuntimeAdequate

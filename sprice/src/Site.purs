2faf50773a026eef564e9a982fb48ac893c4e44b
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-10-20 17:59:00 +0200

    add reading of site URL parameters

---
+++ sprice/src/Site.purs
@@ -0,0 +1,32 @@
+module Sprice.Site (getUrlParam) where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Traversable (oneOf)
+import Effect (Effect)
+import Sprice.QueryString (QueryParam(..))
+import Sprice.QueryString (parseQueryString) as QueryString
+import Web.HTML (window)
+import Web.HTML.Location (search) as Location
+import Web.HTML.Window as Window
+
+getUrlParam :: String -> Effect (Maybe String)
+getUrlParam targetKey = do
+  queryString <- window >>= Window.location >>= Location.search
+  queryString
+    # QueryString.parseQueryString
+    >>> map matchQueryParam
+    >>> oneOf
+    >>> pure
+  where
+  matchQueryParam :: Either String QueryParam -> Maybe String
+  matchQueryParam eQueryParam =
+    case eQueryParam of
+      Right (QueryParam key (Just value)) ->
+        if key == targetKey then
+          Just value
+        else
+          Nothing
+      _ -> Nothing

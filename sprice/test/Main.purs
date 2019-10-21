92bb327a03ebf9328e1c02cc37c63804d340a3fb
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-10-21 20:35:35 +0200

    add QueryString tests

--- sprice/test/Main.purs
+++ sprice/test/Main.purs
@@ -5,9 +5,11 @@ import Prelude
 import Effect (Effect)
 import Test.RuntimeDependency (tests) as RuntimeDependency
 import Test.Unit.Main (runTest)
+import Test.QueryString (tests) as QueryString
 import Test.User (tests) as User
 
 main :: Effect Unit
 main = runTest do
   RuntimeDependency.tests
   User.tests
+  QueryString.tests

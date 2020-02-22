6fdd324c24bef4e4c4aa51467365a824025574c9
Niclas Blomberg <niclas.blomberg@reaktor.com>
2020-02-22 15:18:47 +0100

    add inference function

--- api/src/SweetSpot/Calc.hs
+++ api/src/SweetSpot/Calc.hs
@@ -1,188 +1,61 @@
-{-# LANGUAGE FlexibleContexts #-}
-
 module SweetSpot.Calc
-  -- (enhanceDBStats)
+  ( InfParams
+  , runInference
+  )
 where
 
--- import Control.Lens
--- import Control.Monad.IO.Class (liftIO, MonadIO)
--- import Control.Monad.Reader.Class (MonadReader)
--- import qualified Data.List as L
--- import Data.Map.Strict as M
--- import Data.Maybe (fromJust)
--- import Data.Scientific (toRealFloat)
--- import qualified Data.Vector.Unboxed as V
--- import Statistics.Types (cl95, Estimate, ConfInt, cl90)
--- import Statistics.Resampling (Estimator(..), resample)
--- import Statistics.Resampling.Bootstrap (bootstrapBCA)
--- import System.Random.MWC (createSystemRandom, GenIO)
--- import SweetSpot.AppM
--- import SweetSpot.Data.Common (Svid, BucketType(..), Price(..), UserId)
--- import SweetSpot.Data.Domain
--- import SweetSpot.Data.Api
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed (Vector, (!))
+import qualified Data.List as L
+import Statistics.Resampling
+import Statistics.Sample (mean)
+import System.Random.MWC (createSystemRandom, GenIO, uniformR)
 
--- conversionRate :: Int -> Int -> Double
--- conversionRate conversions users =
---   fromIntegral conversions / fromIntegral users
+data InfParams = InfParams
+  { _conversions :: Vector Double
+  , _nilCount :: Int
+  } deriving (Show)
 
--- findType :: BucketType -> [BucketStats] -> BucketStats
--- findType t stats = fromJust $ L.find ((== t) . (^. bsBucketType)) stats
+data InfResult = InfResult
+  { _lowerBound :: Double
+  , _upperBound :: Double
+  , _mean :: Double
+  } deriving (Show)
 
--- instance Semigroup Double where
---   (<>) = (+)
+nBoot = 1000
 
--- -- Merge multiple checkouts for same user
--- groupByUser :: Semigroup a => [(UserId, a)] -> [(UserId, a)]
--- groupByUser =  M.toList . M.fromListWith (<>)
+getSample :: InfParams -> Vector Double
+getSample (InfParams cs nils) = cs <> V.replicate nils 0.0
 
--- -- Get actual revenue from LineItems
--- calcRev :: Svid -> Double -> [(UserId, [LineItem])] -> [(UserId, Double)]
--- calcRev svid p = mapped . _2 %~ revenue . getMatchingPurchases
---   where
---     getMatchingPurchases = L.filter ((== svid) . (^. liVariantId))
---     revenue lis =
---         lis ^.. traverse . liQuantity
---           & sum
---           & \qty -> fromIntegral qty * p
+randomElement :: GenIO -> Vector Double -> IO Double
+randomElement gen v = do
+  idx <- uniformR (0, (V.length v) - 1) gen
+  return $ v ! idx
 
--- sampleForBucketType :: BucketType -> [BucketStats] -> [Double]
--- sampleForBucketType t bs =
---   L.filter ((== t) . (^. bsBucketType)) bs
---     & (>>= (^. bsUserRevenues))
---     & groupByUser
---     & fmap snd
+compareRandomPair :: GenIO -> Vector Double -> Vector Double -> IO Double
+compareRandomPair gen cMeans tMeans = do
+  cMean <- randomElement gen cMeans
+  tMean <- randomElement gen tMeans
+  return $ tMean / cMean
 
--- enhanceDBBucketStats :: DBBucketStats -> BucketStats
--- enhanceDBBucketStats stats =
---     BucketStats
---         { _bsBucketId = stats ^. dbsBucketId
---         , _bsBucketType = stats ^. dbsBucketType
---         , _bsUserCount = stats ^. dbsUserCount
---         , _bsImpressionCount = stats ^. dbsImpressionCount
---         , _bsPrice = stats ^. dbsPrice
---         , _bsUserRevenues = userRevenues
---         }
---     where
---       (Price p) = stats ^. dbsPrice
---       testVariantId = stats ^. dbsTestSvid
---       -- | TODO Maybe ping shopify to check fulfillment status
---       userRevenues :: [(UserId, Double)]
---       userRevenues = stats ^. dbsCheckoutEvents
---         & fmap (\ev -> (ev ^. chkUserId, ev ^. chkLineItems))
---         & groupByUser
---         & calcRev testVariantId (toRealFloat p)
---         & L.filter ((> 0) . snd)
+runInference :: InfParams -> InfParams -> IO InfResult
+runInference cParams tParams = do
+  gen <- createSystemRandom
+  cMeans <- resample' gen (getSample cParams)
+  tMeans <- resample' gen (getSample tParams)
+  lifts <- mapM (const $ compareRandomPair gen cMeans tMeans) [0..nBoot]
+  let
+    sorted = V.fromList $ L.sort lifts
+    len = V.length sorted
+    nTails = floor $ fromIntegral len * 0.05
+    middle90 = V.slice nTails (len - nTails) sorted
 
--- enhanceDBExperimentStats :: DBExperimentStats -> ExperimentStats
--- enhanceDBExperimentStats stats =
---   let buckets = stats ^. desBuckets
---       totalUserCount = buckets ^.. traverse . dbsUserCount & sum
---       totalImpressionCount = buckets ^.. traverse . dbsImpressionCount & sum
---       enhancedBucketStats = enhanceDBBucketStats <$> stats ^. desBuckets
---       -- Assumes one control and test respectively
---       control = findType Control enhancedBucketStats
---       test = findType Test enhancedBucketStats
+  return InfResult
+    { _mean = mean sorted
+    , _lowerBound = V.head middle90
+    , _upperBound = V.last middle90
+    }
 
---   in ExperimentStats
---         { _esExpId = stats ^. desExpId
---         , _esUserCount = totalUserCount
---         , _esImpressionCount = totalImpressionCount
---         , _esBuckets = enhancedBucketStats
---         }
-
--- enhanceDBStats
---   :: (MonadIO m, MonadReader AppCtx m)
---   => DBCampaignStats
---   -> m CampaignStats
--- enhanceDBStats stats = do
---     gen <- liftIO createSystemRandom
---     controlEstimate <- liftIO $ bootstrap gen controlSample
---     testEstimate <- liftIO $ bootstrap gen testSample
---     return CampaignStats
---         { _csCampaignId = stats ^. dcsCampaignId
---         , _csCampaignName = stats ^. dcsCampaignName
---         , _csMinProfitIncrease = stats ^. dcsMinProfitIncrease
---         , _csStartDate = stats ^. dcsStartDate
---         , _csEndDate = stats ^. dcsEndDate
---         , _csExperiments = []
---         , _csProfitPerUserControl = controlEstimate
---         , _csProfitPerUserTest = testEstimate
---         , _csConvertersControl = convertersControl
---         , _csNonConvertersControl = nonConvertersCountControl
---         , _csConvertersTest = convertersTest
---         , _csNonConvertersTest = nonConvertersCountTest
---         , _csConvertersControlCount = convertersCountControl
---         , _csConvertersTestCount = convertersCountTest
---         , _csConversionRateControl =
---             conversionRate convertersCountControl (convertersCountControl + nonConvertersCountControl)
---         , _csConversionRateTest =
---             conversionRate convertersCountTest (convertersCountTest + nonConvertersCountTest)
---         }
---   where
---     enhancedExperiments =
---       stats ^. dcsExperiments ^.. traverse & fmap enhanceDBExperimentStats
---     bucketStats =
---       enhancedExperiments ^.. traverse . esBuckets & mconcat
-
---     convertersControl = sampleForBucketType Control bucketStats
---     convertersTest = sampleForBucketType Test bucketStats
-
---     nonConvertersControl = nonConvertersForBucketType Control bucketStats
---     nonConvertersTest = nonConvertersForBucketType Test bucketStats
-
---     convertersCountControl = L.length convertersControl
---     convertersCountTest = L.length convertersTest
-
---     nonConvertersCountControl = L.length nonConvertersControl
---     nonConvertersCountTest = L.length nonConvertersTest
-
---     controlSample = V.fromList $ convertersControl <> nonConvertersControl
---     testSample = V.fromList $ convertersTest <> nonConvertersTest
-
---     nonConvertersForBucketType :: BucketType -> [BucketStats] -> [Double]
---     nonConvertersForBucketType t bs =
---       bs
---         & L.filter ((== t) . (^. bsBucketType))
---         & ((^. bsUserCount) . (!! 0))
---         & flip (-) convs
---         & flip L.take (repeat 0.0)
---       where
---         convs = case t of
---           Control -> L.length convertersControl
---           Test -> L.length convertersTest
-
--- bootstrap :: GenIO -> V.Vector Double -> IO (Estimate ConfInt Double)
--- bootstrap gen sample = do
---   resampled <- resample gen [Mean] 1000 sample
---   return $ head $ bootstrapBCA cl90 sample resampled
-
-
-
--- -- | ---------------------------------------------------------------------------
--- -- | Simulation
--- -- | ---------------------------------------------------------------------------
--- data SimParams = SimParams
---   { _size :: Int
---   , _cr :: Double
---   , _avgUsrProfit :: Double
---   }
-
--- genSample :: SimParams -> V.Vector Double
--- genSample params = convs <> nonConvs
---   where
---     convCount = round (_cr params * fromIntegral (_size params))
---     nonConvCount = _size params - convCount
---     convs = V.fromList $ L.replicate convCount (_avgUsrProfit params)
---     nonConvs = V.fromList $ L.replicate nonConvCount 0.0
-
--- simulate :: SimParams
---          -> SimParams
---          -> IO ()
--- simulate c t = do
---   gen <- createSystemRandom
---   controlRes <- bootstrap gen $ genSample c
---   testRes <- bootstrap gen $ genSample t
---   print controlRes
---   putStrLn "==="
---   print testRes
---   return ()
+  where
+    resample' gen s =
+      resamples . snd . L.head <$> resample gen [Mean] 1000 s

c1d389eb4a54bbaaf348f49ce022c47ad798597f
Niclas Blomberg <nicke.blomberg@gmail.com>
2020-03-23 17:25:53 +0100

    wip: register conversions

--- api/src/SweetSpot/Route/Webhook.hs
+++ api/src/SweetSpot/Route/Webhook.hs
@@ -9,11 +9,17 @@ module SweetSpot.Route.Webhook
   , webhookHandler
   ) where
 
+import Control.Applicative (liftA2)
+import Control.Lens
 import Data.Aeson (Value)
+import Data.Aeson.Lens
+import Data.Maybe (catMaybes)
 import qualified Data.Text as T
+import Data.Text.Lens (packed)
 import Servant
 import SweetSpot.AppM (ServerM, AppM(..))
 import SweetSpot.Data.Api (OkResponse(..))
+import SweetSpot.Data.Common (Svid(..))
 import qualified SweetSpot.Logger as L
 
 type CheckoutRoute = "webhook" :> "checkout"
@@ -24,7 +30,19 @@ type WebhookAPI = CheckoutRoute
 
 checkoutHandler :: Value -> ServerM OkResponse
 checkoutHandler v = runAppM $ do
-  L.info . T.pack . show $ v
+  let
+    conversions :: [(Svid, Integer)]
+    conversions =
+      v ^.. key "line_items"
+      . values
+      . to (\li ->
+                liftA2 (,)
+                  (li ^? key "variant_id" . _Integer . to show . packed . to Svid)
+                  (li ^? key "quantity" . _Integer)
+           )
+      & catMaybes
+
+  L.info . T.pack . show $ conversions
   return OkResponse { message = "Received checkout" }
 
 webhookHandler = checkoutHandler

50a80e709c76434d696317dc9086db93d2af3781
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-04-11 09:32:51 +0200

    Don't try to decode error responses

--- dashboard/src/SweetSpot/Service.purs
+++ dashboard/src/SweetSpot/Service.purs
@@ -1,9 +1,8 @@
 module SweetSpot.Service where
 
 import Prelude
-
 import Data.Argonaut (decodeJson, jsonParser, Json)
-import Data.Either (Either)
+import Data.Either (Either(..))
 import Effect.Aff (Aff)
 import Milkis (Options, Response, Fetch)
 import Milkis as Milkis
@@ -27,20 +26,20 @@ getJson url options = fetch (Milkis.URL url) combinedOptions
   combinedOptions :: Record Options
   combinedOptions = RecordUnsafe.unsafeUnion options defaults
 
-fetchThing
-  :: forall t.
-     String
-  -> (Json -> Either String t)
-  -> Aff (Either String t)
+fetchThing :: forall t. String -> (Json -> Either String t) -> Aff (Either String t)
 fetchThing route decoder = do
   res <- getJson route {}
   text <- Milkis.text res
-  pure (jsonParser text >>= decodeJson >>= decoder)
+  let
+    status = Milkis.statusCode res
+  pure
+    if status == 200 || status == 201 then
+      (jsonParser text >>= decodeJson >>= decoder)
+    else
+      (Left (show status <> " - " <> text))
 
 fetchCampaigns :: SessionId -> Aff (Either String (Array UICampaign))
-fetchCampaigns (SessionId session) =
-  fetchThing ("/api/dashboard/campaigns?session=" <> session) Codec.decodeUICampaigns
+fetchCampaigns (SessionId session) = fetchThing ("//localhost:8082/api/dashboard/campaigns?session=" <> session) Codec.decodeUICampaigns
 
 fetchProducts :: SessionId -> Aff (Either String (Array Product))
-fetchProducts (SessionId session) =
-  fetchThing ("/api/dashboard/products?session=" <> session) Codec.decodeProducts
+fetchProducts (SessionId session) = fetchThing ("//localhost:8082/api/dashboard/products?session=" <> session) Codec.decodeProducts

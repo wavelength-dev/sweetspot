ed87c854adc86c7a2d19431af058bc88d0a2c881
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-04-25 14:13:41 +0200

    Update build to rebuild cache image

---
+++ check-should-rebuild-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+created=$(docker inspect -f '{{.Created}}' gcr.io/$PROJECT_ID/sweetspot-build)
+last_run=$(date -d$created +%s)
+now=$(date +%s)
+seconds_since_last_run=$(($now - $last_run))
+seconds_in_day=86400
+
+if [ $seconds_since_last_run -gt $seconds_in_day ]; then
+  # We should rebuild the image
+  echo "We should rebuild the image, exiting with success to continue build"
+  exit 0
+else
+  echo "Shouldn't rebuild the image yet, exiting with error to stop build"
+  exit 1
+fi

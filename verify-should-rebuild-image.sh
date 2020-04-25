71e095f9784c934a763d4ca55c1cd6c2ae020102
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-04-25 15:05:37 +0200

    Simplify get docker cli

--- verify-should-rebuild-image.sh
+++ verify-should-rebuild-image.sh
@@ -2,9 +2,7 @@
 set -e
 
 # get docker cli
-apk add curl
-curl -LOs https://github.com/docker/cli/archive/v19.03.8.tar.gz
-tar -xzf ./v19.03.8.tar.gz
+apk add docker
 
 created=$(./docker inspect -f '{{.Created}}' gcr.io/$PROJECT_ID/sweetspot-build)
 last_run=$(date -d$created +%s)

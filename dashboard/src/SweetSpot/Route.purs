df782fc44b113c1ee82cf4d0fe824b5d68ed1db5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-04-26 12:39:58 +0200

    wip browser routing
    Adds query strings but fails to make them work with browser history.

--- dashboard/src/SweetSpot/Route.purs
+++ dashboard/src/SweetSpot/Route.purs
@@ -1,33 +1,53 @@
-module SweetSpot.Route
-  ( Route(..)
-  , hoistRouter
-  ) where
-
-import Prelude
+module SweetSpot.Route where
 
 import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe, fromMaybe)
+import Data.Symbol (SProxy(..))
 import Effect (Effect)
-import Routing.Duplex (RouteDuplex', path, root, segment, string, parse)
-import Routing.Duplex.Generic as G
+import Prelude (class Eq, class Show, Unit, (#), ($), (<>))
+import Routing.Duplex (RouteDuplex', optional, param, parse, path, record, root, segment, (:=))
+import Routing.Duplex.Generic (product, sum)
 import Routing.Hash (matchesWith)
 
-data Route = Home | Campaign String
+-- toHistoryState :: { page :: Route } -> { page :: String }
+-- toHistoryState { page } = case page of
+--   Home -> { page: "home" }
+--   Campaign campaignId -> { page: "campaign-" <> campaignId }
+
+-- useEffect route do
+--   Console.log $ "new route: " <> show route
+--   History.pushState
+--     (toHistoryState >>> Foreign.unsafeToForeign $ { page: route })
+--     (DocumentTitle "SweetSpot Dashboard")
+--     (URL currentUrl)
+--     history
+--   pure mempty
+
+data Route
+  = Home { session :: Maybe String }
+  | Campaign String { session :: Maybe String }
 
 instance showRoute :: Show Route where
-  show Home = "Home"
-  show (Campaign campaignId) = "Campaign" <> campaignId
+  show (Home { session }) = "Home " <> ", session: " <> (fromMaybe "missing session" session)
+  show (Campaign campaignId { session }) = "Campaign" <> campaignId <> (fromMaybe "messing session" session)
 
 derive instance genericRoute :: Generic Route _
 
-route :: RouteDuplex' Route
-route = root $ G.sum
-  { "Home": G.noArgs
-  , "Campaign": path "campaign" (string segment)
-  }
+derive instance eqRoute :: Eq Route
 
-hoistRouter ::  (Route -> Effect Unit) -> Effect (Effect Unit)
+routeCodec :: RouteDuplex' Route
+routeCodec =
+  root
+    $ sum
+        { "Home": (record # _session := optional (param "session"))
+        -- , "Campaign": (path "campaign" ? { session: optional })
+        , "Campaign": product (path "campaign" segment) (record # _session := optional (param "session"))
+        }
+  where
+  _session = SProxy :: SProxy "session"
+
+hoistRouter :: (Route -> Effect Unit) -> Effect (Effect Unit)
 hoistRouter setRoute =
-  matchesWith (parse route) \_ newRoute ->
-    case newRoute of
-      Home -> setRoute Home
-      Campaign campaignId -> setRoute (Campaign campaignId)
+  matchesWith (parse routeCodec) \_ newRoute -> case newRoute of
+    Home sessionId -> setRoute (Home sessionId)
+    Campaign campaignId sessionId -> setRoute (Campaign campaignId sessionId)

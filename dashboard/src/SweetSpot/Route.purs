b8fe6d818e37ffe0c29b7b7a834e38290130d8a8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-05-09 14:26:59 +0200

    fix router
    Updates routes and their encoded paths.
    Sets the route whenever the hash is updated and successfully parsed.

--- dashboard/src/SweetSpot/Route.purs
+++ dashboard/src/SweetSpot/Route.purs
@@ -1,35 +1,22 @@
 module SweetSpot.Route where
 
 import Data.Generic.Rep (class Generic)
-import Data.Maybe (Maybe, fromMaybe)
-import Data.Symbol (SProxy(..))
 import Effect (Effect)
-import Prelude (class Eq, class Show, Unit, (#), ($), (<>))
-import Routing.Duplex (RouteDuplex', optional, param, parse, path, record, root, segment, (:=))
-import Routing.Duplex.Generic (product, sum)
+import Prelude (class Eq, class Show, Unit, bind, pure, ($), (<>))
+import Routing.Duplex (RouteDuplex', parse, root, segment)
+import Routing.Duplex.Generic (noArgs, sum)
+import Routing.Duplex.Generic.Syntax ((/))
 import Routing.Hash (matchesWith)
 
--- toHistoryState :: { page :: Route } -> { page :: String }
--- toHistoryState { page } = case page of
---   Home -> { page: "home" }
---   Campaign campaignId -> { page: "campaign-" <> campaignId }
-
--- useEffect route do
---   Console.log $ "new route: " <> show route
---   History.pushState
---     (toHistoryState >>> Foreign.unsafeToForeign $ { page: route })
---     (DocumentTitle "SweetSpot Dashboard")
---     (URL currentUrl)
---     history
---   pure mempty
-
 data Route
-  = CampaignList { session :: Maybe String }
-  | Campaign String { session :: Maybe String }
+  = CampaignList
+  | CampaignView String
+  | CampaignCreate
 
 instance showRoute :: Show Route where
-  show (CampaignList { session }) = "Campaign list" <> ", session: " <> (fromMaybe "missing session" session)
-  show (Campaign campaignId { session }) = "Campaign" <> campaignId <> (fromMaybe "messing session" session)
+  show (CampaignList) = "Experiment List"
+  show (CampaignView campaignId) = "Experiment View: " <> campaignId
+  show (CampaignCreate) = "Experiment Create"
 
 derive instance genericRoute :: Generic Route _
 
@@ -39,15 +26,13 @@ routeCodec :: RouteDuplex' Route
 routeCodec =
   root
     $ sum
-        { "CampaignList": path "campaign-list" (record # _session := optional (param "session"))
-        -- , "Campaign": (path "campaign" ? { session: optional })
-        , "Campaign": product (path "campaign" segment) (record # _session := optional (param "session"))
+        { "CampaignList": noArgs
+        , "CampaignView": "campaign" / segment
+        , "CampaignCreate": "campaign" / noArgs
         }
-  where
-  _session = SProxy :: SProxy "session"
 
-hoistRouter :: (Route -> Effect Unit) -> Effect (Effect Unit)
-hoistRouter setRoute =
-  matchesWith (parse routeCodec) \_ newRoute -> case newRoute of
-    CampaignList sessionId -> setRoute (CampaignList sessionId)
-    Campaign campaignId sessionId -> setRoute (Campaign campaignId sessionId)
+useRouter :: (Route -> Effect Unit) -> Effect (Effect Unit)
+useRouter setRoute = do
+  removeHashEventListener <-
+    matchesWith (parse routeCodec) \oldRoute newRoute -> setRoute newRoute
+  pure removeHashEventListener

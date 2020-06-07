40c6cf6f8e9541e5c67facab38e37ad506bcda58
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-06-07 11:27:35 +0200

    clean up unused bits in logging

--- fulcrum/src/Logging.purs
+++ fulcrum/src/Logging.purs
@@ -1,9 +1,6 @@
 module Fulcrum.Logging (log, LogLevel(..)) where
 
 import Prelude
-
-import Data.Argonaut (class EncodeJson)
-import Data.Argonaut (encodeJson) as Argonaut
 import Data.Maybe as Maybe
 import Effect (Effect)
 import Datadog (logError, logInfo, logWarn) as Datadog
@@ -15,16 +12,6 @@ data LogLevel
   | Warn
   | Error
 
-instance showLogLevel :: Show LogLevel where
-  show Info = "info"
-  show Warn = "warn"
-  show Error = "error"
-
-instance encodeJsonLogLevel :: EncodeJson LogLevel where
-  encodeJson Info = Argonaut.encodeJson "info"
-  encodeJson Warn = Argonaut.encodeJson "warn"
-  encodeJson Error = Argonaut.encodeJson "error"
-
 log :: forall a. Show a => LogLevel -> a -> Effect Unit
 log level message = do
   -- for convenience one can add a query parameter ssdebug to see logs
@@ -35,9 +22,9 @@ log level message = do
     Warn -> message # show >>> Console.warn
     Error -> message # show >>> Console.error
   case level of
-       Info -> message # show >>> Datadog.logInfo
-       Warn -> message # show >>> Datadog.logWarn
-       Error -> message # show >>> Datadog.logError
+    Info -> message # show >>> Datadog.logInfo
+    Warn -> message # show >>> Datadog.logWarn
+    Error -> message # show >>> Datadog.logError
 
 getIsDebugging :: Effect Boolean
 getIsDebugging = Site.getUrlParam "ssdebug" >>= Maybe.isJust >>> pure

bcf318bd775000dee56e9a814fa8d7a25ab77907
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-06-07 14:12:04 +0200

    refine logging to work with a JS object

--- dashboard/src/Datadog.purs
+++ dashboard/src/Datadog.purs
@@ -2,28 +2,30 @@ module Datadog where
 
 import Effect (Effect)
 import Effect.Uncurried (EffectFn2, runEffectFn2)
+import Foreign.Object (Object)
+import Foreign.Object (empty) as Object
 import Prelude (Unit)
 
-foreign import logInfoImpl :: forall a. EffectFn2 String (Record a) Unit
+foreign import logInfoImpl :: forall a. EffectFn2 String (Object a) Unit
 
-foreign import logWarnImpl :: forall a. EffectFn2 String (Record a) Unit
+foreign import logWarnImpl :: forall a. EffectFn2 String (Object a) Unit
 
-foreign import logErrorImpl :: forall a. EffectFn2 String (Record a) Unit
+foreign import logErrorImpl :: forall a. EffectFn2 String (Object a) Unit
 
 logInfo :: String -> Effect Unit
-logInfo msg = runEffectFn2 logInfoImpl msg {}
+logInfo msg = runEffectFn2 logInfoImpl msg Object.empty
 
 logWarn :: String -> Effect Unit
-logWarn msg = runEffectFn2 logWarnImpl msg {}
+logWarn msg = runEffectFn2 logWarnImpl msg Object.empty
 
 logError :: String -> Effect Unit
-logError msg = runEffectFn2 logErrorImpl msg {}
+logError msg = runEffectFn2 logErrorImpl msg Object.empty
 
-logInfoContext :: forall a. String -> Record a -> Effect Unit
+logInfoContext :: forall a. String -> Object a -> Effect Unit
 logInfoContext = runEffectFn2 logInfoImpl
 
-logWarnContext :: forall a. String -> Record a -> Effect Unit
+logWarnContext :: forall a. String -> Object a -> Effect Unit
 logWarnContext = runEffectFn2 logWarnImpl
 
-logErrorContext :: forall a. String -> Record a -> Effect Unit
+logErrorContext :: forall a. String -> Object a -> Effect Unit
 logErrorContext = runEffectFn2 logErrorImpl

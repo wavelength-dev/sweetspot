cc051da0213679f137cdce4e3f0a2dd9a2d5e913
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-06-27 14:18:08 +0200

    remove unneeded base image update script
    Builds now cache image layers automagically. Thanks Kaniko 🙏.

--- update-cached-base-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-docker build --tag sweetspot-build --file Dockerfile.base .
-docker tag sweetspot-build gcr.io/sweetspot-255522/sweetspot-build
-docker push gcr.io/sweetspot-255522/sweetspot-build

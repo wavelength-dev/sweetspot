9fa9f598d187b25b13dbd0e58ef9ed64a6fd1cc6
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-07-12 13:53:38 +0200

    add dependency required for css variables

--- dashboard/.postcssrc.js
+++ dashboard/.postcssrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  "modules": true,
-  "plugins": {
-    "postcss-custom-media": true
-  }
+  modules: true,
+  plugins: {
+    'postcss-custom-media': true,
+    'postcss-custom-properties': true,
+  },
 }

b651f2d4e72fa7d3e7af02ab27837020fbacb2a8
Niclas Blomberg <nicke.blomberg@gmail.com>
2020-07-19 15:42:46 +0200

    link to getting started from first page

--- dashboard/src/SweetSpot/GettingStartedPage.purs
+++ dashboard/src/SweetSpot/GettingStartedPage.purs
@@ -2,7 +2,7 @@ module SweetSpot.GettingStartedPage where
 
 import Assets (mountainCode) as Assets
 import Data.Nullable (notNull)
-import React.Basic.DOM (text) as R
+import React.Basic.DOM (text, p, a, css) as R
 import React.Basic.Hooks (JSX, element)
 import SweetSpot.Shopify (emptyState) as Shopify
 
@@ -12,5 +12,11 @@ gettingStartedPage =
     { heading: "Discover more profitable prices for your products"
     , action: notNull { content: "Create Price Experiment", url: "#/create" }
     , image: Assets.mountainCode
-    , children: [ R.text "Here you'll create new price tests, check their progress, or their outcome." ]
+    , children: [
+      R.p {children: [ R.text "Here you'll create new price tests, check their progress, or their outcome." ]},
+      R.p {
+        style: R.css { marginTop: "1em"},
+        children: [ R.text "Before creating your first experiment, please make sure to read the ", R.a { target: "_blank", href: "https://getsweetspot.com/getting-started", children: [R.text "getting started guide."]} ]
+        }
+    ]
     }

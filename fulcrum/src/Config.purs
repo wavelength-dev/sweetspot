6ed97cc6e9241a64532c256b8ecf0901c241e739
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-08-30 13:05:02 +0200

    Remove sending cart tokens

--- fulcrum/src/Config.purs
+++ fulcrum/src/Config.purs
@@ -3,9 +3,6 @@ module Fulcrum.Config where
 apiUrl :: String
 apiUrl = "/apps/sweetspot/api/fulcrum"
 
-tokenStashKey :: String
-tokenStashKey = "sweetspot_cart_tokens"
-
 dryRunMap :: String -> Boolean
 dryRunMap = case _ of
   "libertyprice.myshopify.com" -> true

e6b36f63cf1a4549025ec6daef41afa7feae76e6
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-09-05 12:37:27 +0200

    Simplify

--- fulcrum/src/Main.purs
+++ fulcrum/src/Main.purs
@@ -2,7 +2,7 @@ module Fulcrum.Main where
 
 import Prelude
 import Control.Monad.Cont (lift)
-import Control.Monad.Except (ExceptT, runExceptT, throwError)
+import Control.Monad.Except (ExceptT, except, runExceptT, throwError)
 import Data.Either (Either(..))
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
@@ -32,12 +32,8 @@ getTestMap :: UserId -> ExceptT String Aff TestMapByVariant
 getTestMap userId = do
   isRuntimeAdequate <- liftEffect RuntimeDependency.getIsRuntimeAdequate
   when (not isRuntimeAdequate) (throwError inadequateRuntimeError)
-  let
-    payload = OnlyUserId userId
-  eTestMaps <- lift $ Service.fetchTestMaps payload
-  case eTestMaps of
-    Left msg -> throwError msg
-    Right testMaps -> testMaps # Data.hashMapFromTestMaps >>> pure
+  testMaps <- Service.fetchTestMaps (OnlyUserId userId) # lift >>= except
+  Data.hashMapFromTestMaps testMaps # pure
   where
   inadequateRuntimeError = "sweetspot can't run in current runtime"
 
@@ -59,34 +55,32 @@ main :: Effect Unit
 main =
   Aff.runAff_ wrapUp do
     exposeGlobals reapply # liftEffect
-    hostname <- Site.readHostname # liftEffect
-    isDryRun <- Site.getIsDryRun # liftEffect
-    isDebugging <- Site.getIsDebugging # liftEffect
-    isPricePage <- Site.getIsPricePage # liftEffect
-    case isPricePage of
-      false -> mempty
-      true -> do
-        liftEffect $ Logger.logWithContext Info ("running fulcrum on " <> hostname) { isDryRun, isDebugging }
-        eUserId <- findUserIdWithWaitLimit
-        case eUserId of
-          Left msg -> throwError $ Aff.error msg
-          Right userId -> do
-            sessionTestContext <-
-              liftEffect do
-                RunState.initRunQueue
-                RunState.initTestContext
-                RunState.getTestContext
-            eTestContext <- runExceptT $ getTestMap userId
-            case eTestContext of
-              Left msg -> throwError (Aff.error msg)
-              -- we cache the test maps and apply them
-              Right testContext -> do
-                unless (Map.isEmpty testContext) do
-                  -- as this is the main loop, and it only runs once, we can safely assume the avar to be empty
-                  _ <- AAVar.tryPut testContext sessionTestContext
-                  applyTestMaps testContext # liftEffect
-                  observeTestPrices testContext # liftEffect
-                  observeCheckout testContext # liftEffect
+    hostname <- liftEffect Site.readHostname
+    isDryRun <- liftEffect Site.getIsDryRun
+    isDebugging <- liftEffect Site.getIsDebugging
+    awaitDomReady
+    isPricePage <- liftEffect Site.getIsPricePage
+    when isPricePage do
+      Logger.logWithContext Info ("running fulcrum on " <> hostname) { isDryRun, isDebugging } # liftEffect
+      eSuccess <-
+        runExceptT do
+          userId <- findUserIdWithWaitLimit # lift >>= except
+          sessionTestContext <-
+            liftEffect do
+              RunState.initRunQueue
+              RunState.initTestContext
+              RunState.getTestContext
+          testContext <- getTestMap userId
+          -- we cache the test maps and apply them
+          unless (Map.isEmpty testContext) do
+            -- as this is the main loop, and it only runs once, we can safely assume the avar to be empty
+            _ <- AAVar.tryPut testContext sessionTestContext # lift
+            applyTestMaps testContext # liftEffect
+            observeTestPrices testContext # liftEffect
+            observeCheckout testContext # liftEffect
+      case eSuccess of
+        Left msg -> Aff.error msg # throwError
+        Right _ -> mempty
 
 applyTestMaps :: TestMapByVariant -> Effect Unit
 applyTestMaps testMap = TestPrice.applyTestPrices testMap *> Checkout.applyTestCheckout testMap
@@ -128,17 +122,15 @@ reapply :: Effect Unit
 reapply = queueNext applyDynamicPrice
 
 findUserIdWithWaitLimit :: Aff (Either String UserId)
-findUserIdWithWaitLimit =
-  let
-    tryFindUserId waitTime = do
-      mUserId <- liftEffect User.findUserId
-      case mUserId of
-        Just userId -> pure $ Right userId
-        Nothing ->
-          if waitTime > 6000.0 then
-            "find user id timed out" # Left >>> pure
-          else do
-            Aff.delay (Milliseconds 50.0)
-            tryFindUserId (waitTime + 50.0)
-  in
-    tryFindUserId 0.0
+findUserIdWithWaitLimit = tryFindUserId 0.0
+  where
+  tryFindUserId waitTime = do
+    mUserId <- liftEffect User.findUserId
+    case mUserId of
+      Just userId -> Right userId # pure
+      Nothing ->
+        if waitTime > 6000.0 then
+          Left "find user id timed out" # pure
+        else do
+          Aff.delay (Milliseconds 50.0)
+          tryFindUserId (waitTime + 50.0)

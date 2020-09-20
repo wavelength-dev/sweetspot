5a6c96a9ca5d237c285029bac0fafce1ea01b5c4
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-09-20 20:23:50 +0200

    Revert "Safe testmap caching version"
    This reverts commit 9da6c1f2fcc11726871502041cf0d3a2cfba9af3.

--- fulcrum/src/Cache.purs
+++ fulcrum/src/Cache.purs
@@ -1,17 +1,13 @@
 module Fulcrum.Cache where
 
 import Prelude
-import Data.Argonaut (class DecodeJson, class EncodeJson, Json, JsonDecodeError(..))
-import Data.Argonaut (decodeJson, encodeJson, fromNumber, jsonParser, printJsonDecodeError, stringify, toNumber) as Argonaut
-import Data.Bifunctor (lmap)
+import Data.Argonaut (class DecodeJson, class EncodeJson)
+import Data.Argonaut (decodeJson, encodeJson, fromString, jsonParser, stringify, toString) as Argonaut
 import Data.DateTime (DateTime) as D
-import Data.DateTime.Instant as Instant
 import Data.Either (Either, note)
-import Data.Maybe (Maybe)
-import Data.Newtype (class Newtype)
-import Data.Newtype (unwrap) as Newtype
-import Data.Time.Duration (Milliseconds(..))
+import Data.JSDate (fromDateTime, parse, toDateTime, toISOString) as JSDate
 import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
 import Fulcrum.Data (TestMap)
 import Web.HTML (window) as HTML
 import Web.HTML.Window (localStorage) as Window
@@ -20,51 +16,40 @@ import Web.Storage.Storage (getItem, setItem) as Storage
 newtype DateTime
   = DateTime D.DateTime
 
-derive instance newtypeDateTime :: Newtype DateTime _
-
 testMapCacheKey :: String
 testMapCacheKey = "sweetspot__test_maps"
 
-instance decodeJsonDateTime :: DecodeJson DateTime where
-  decodeJson json = (toNumber json <#> Milliseconds) >>= toInstant <#> Instant.toDateTime >>> DateTime :: Either JsonDecodeError DateTime
-    where
-    toNumber = Argonaut.toNumber >>> (note (TypeMismatch "timestamp not a number") :: forall a. Maybe a -> Either JsonDecodeError a)
+instance decodeInstant :: DecodeJson DateTime where
+  decodeJson json = do
+    str <- Argonaut.toString json # note "expected string"
+    let
+      jsDate = JSDate.parse str # unsafePerformEffect
+    JSDate.toDateTime jsDate # note "not a valid date" <#> DateTime
 
-    toInstant = Instant.instant >>> note (TypeMismatch "valid timestamp but invalid moment in time")
-
-instance encodeJsonDateTime :: EncodeJson DateTime where
-  encodeJson =
-    Newtype.unwrap >>> Instant.fromDateTime
-      >>> Instant.unInstant
-      >>> Newtype.unwrap
-      >>> Argonaut.fromNumber
+instance encodeDateTime :: EncodeJson DateTime where
+  encodeJson (DateTime dateTime) =
+    JSDate.fromDateTime dateTime
+      # JSDate.toISOString
+      >>> unsafePerformEffect
+      >>> Argonaut.fromString
 
 type CachedMaps
-  = { created :: D.DateTime, testMaps :: Array TestMap }
+  = { created :: DateTime
+    , testMaps :: Array TestMap
+    }
 
-type CachedMaps'
-  = { created :: DateTime, testMaps :: Array TestMap }
-
-decodeCachedTestMaps :: Json -> Either JsonDecodeError CachedMaps
-decodeCachedTestMaps = decodeJson >>> map \{ created, testMaps } -> { created: Newtype.unwrap created, testMaps }
-  where
-  decodeJson :: Json -> Either JsonDecodeError CachedMaps'
-  decodeJson = Argonaut.decodeJson
+decodeCachedTestMaps :: String -> Either String CachedMaps
+decodeCachedTestMaps = Argonaut.jsonParser >=> Argonaut.decodeJson
 
 getCachedTestMaps :: Effect (Either String CachedMaps)
 getCachedTestMaps = do
   localStorage <- HTML.window >>= Window.localStorage
   eRawMaps <- Storage.getItem testMapCacheKey localStorage <#> note "no cached map available"
-  eRawMaps
-    >>= ( Argonaut.jsonParser
-          >=> decodeCachedTestMaps
-          >>> lmap Argonaut.printJsonDecodeError
-      )
-    # pure
+  eRawMaps >>= decodeCachedTestMaps # pure
 
 setCachedTestMaps :: CachedMaps -> Effect Unit
-setCachedTestMaps { created, testMaps } = do
+setCachedTestMaps testMaps = do
   localStorage <- HTML.window >>= Window.localStorage
   let
-    jsonStr = Argonaut.encodeJson { created: DateTime created, testMaps } # Argonaut.stringify
-  Storage.setItem testMapCacheKey jsonStr localStorage
+    str = Argonaut.encodeJson testMaps # Argonaut.stringify
+  Storage.setItem testMapCacheKey str localStorage

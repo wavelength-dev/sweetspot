786edea94c7e8392a9de501a1a3425cfc1aa3eb3
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2020-09-28 17:10:44 +0200

    Remove debug log line

--- fulcrum/src/Checkout/Checkout.purs
+++ fulcrum/src/Checkout/Checkout.purs
@@ -17,7 +17,7 @@ import Fulcrum.Checkout.Highlight (highlightCheckout)
 import Fulcrum.Data (TestMapByVariant, VariantId(..))
 import Fulcrum.EstablishedTitles (isCurrentSite) as EstablishedTitles
 import Fulcrum.Logger (LogLevel(..))
-import Fulcrum.Logger (log, logWithContext) as Logger
+import Fulcrum.Logger (logWithContext) as Logger
 import Fulcrum.Site (getIsDebugging, getIsDryRun, getUrlParam, onElementsMutation, queryDocument, readHostname) as Site
 import Unsafe.Coerce (unsafeCoerce)
 import Web.DOM (Element)
@@ -96,7 +96,6 @@ setTestCheckout testMap = do
     execute = do
       traverse_ (setCheckoutVariantId testMap) optionElements
       when ((isEstablishedTitles || isLibertyPrice) && isTestPrice) do
-        Logger.log Info $ "isTestPrice " <> (show isTestPrice)
         setOptionTexts
         setLabelTexts
   unless isDryRun execute
